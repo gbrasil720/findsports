1f42c80923047305fb8d74d027ec65ada0631f4c
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2026-05-18 09:58:02 -0300

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import { createDb } from "@findsports_oficial/db";
+import * as schema from "@findsports_oficial/db/schema/auth";
+import { env } from "@findsports_oficial/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [tanstackStartCookies()],
+  });
+}
+
+export const auth = createAuth();
