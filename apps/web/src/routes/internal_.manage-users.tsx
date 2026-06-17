eac5c4dab7bdf90aef0035ce46bc8e53e3631c36
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2026-06-16 22:24:58 -0300

    wip on posthog

--- apps/web/src/routes/internal_.manage-users.tsx
+++ apps/web/src/routes/internal_.manage-users.tsx
@@ -1,3 +1,4 @@
+/** biome-ignore-all lint/a11y/noLabelWithoutControl: <explanation> */
 import { Badge } from '@findsports_oficial/ui/components/badge'
 import {
   Dialog,
--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -3,7 +3,10 @@ import { z } from 'zod'
 
 export const env = createEnv({
   clientPrefix: 'VITE_',
-  client: {},
+  client: {
+    VITE_POSTHOG_PROJECT_TOKEN: z.string().min(1),
+    VITE_POSTHOG_HOST: z.url()
+  },
   runtimeEnv: (import.meta as any).env,
   emptyStringAsUndefined: true
 })
