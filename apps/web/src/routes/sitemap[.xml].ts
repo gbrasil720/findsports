83e41e6fc0f5ee80d2f48ec4583b9403e64e2a7b
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2026-08-06 18:34:12 -0300

    fix(seo): point crawler files to Onside domain

--- apps/web/src/routes/sitemap[.xml].ts
+++ apps/web/src/routes/sitemap[.xml].ts
@@ -1,17 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-const BASE_URL = 'https://findsports.com.br'
+const BASE_URL = 'https://www.onside.sh'
 
-const PUBLIC_ROUTES = [{ path: '/', priority: '1.0', changefreq: 'weekly' }]
+const PUBLIC_ROUTES = ['/']
 
 function buildSitemap(): string {
-  const now = new Date().toISOString().split('T')[0]
   const urls = PUBLIC_ROUTES.map(
-    ({ path, priority, changefreq }) => `  <url>
+    (path) => `  <url>
     <loc>${BASE_URL}${path}</loc>
-    <lastmod>${now}</lastmod>
-    <changefreq>${changefreq}</changefreq> 
-    <priority>${priority}</priority>
   </url>`
   ).join('\n')
 
